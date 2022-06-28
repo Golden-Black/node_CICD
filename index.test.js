46732269ccc8274b73d035534fe2a88e04e5f1c6
TeamCaptionit <36687847+TeamCaptionit@users.noreply.github.com>
2022-06-28 09:13:51 -0500

    fixed the function name error

--- index.test.js
+++ index.test.js
@@ -6,5 +6,5 @@ test('two plus two is four', () => {
 
 const sub = require('./index');
 test('nine minus one then minus three is five', () =>{
-    expect(add(9, 1, 3)).toBe(5);
+    expect(sub(9, 1, 3)).toBe(5);
 });
