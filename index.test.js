0d2b65eb96cd38d5a22fef245880205a1491e362
TeamCaptionit <36687847+TeamCaptionit@users.noreply.github.com>
2022-06-28 09:06:02 -0500

    Added a test for the subtraction function

--- index.test.js
+++ index.test.js
@@ -3,3 +3,8 @@ const add = require('./index');
 test('two plus two is four', () => {
     expect(add(2, 2)).toBe(4);
   });
+
+const sub = require('./index');
+test('nine minus one then minus three is five', () =>{
+    expect(add(9, 1, 3)).toBe(5);
+});
